fad20af2bb5ae5520a2504c7ac8ae6d037cd5888
MR.Y <programming@yatsukouji.com>
2015-11-23 18:51:47 +0900

    デバイスのキーを設定

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,8 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+
+  config.secret_key = '148029cccbeade600d155658589ccb72e73a91dcc3804ffbec69e9756824fbe3ad2f5b9f052b57debce3cd2d2baaeedf743fa5e4739cef2a1cd22fd569216692'
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
